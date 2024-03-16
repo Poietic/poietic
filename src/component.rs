f461e41a0575a0044f8502673468f78f3999929c
lechos22 <63498252+lechos22@users.noreply.github.com>
2024-03-16 22:12:42 +0100

    Add `render_composition` function

--- src/component.rs
+++ src/component.rs
@@ -1,6 +1,10 @@
 use std::{collections::HashMap, future::Future, pin::Pin, sync::Arc};
+use serde_json::Map;
+pub use serde_json::Value as JsonValue;
 
 use crate::html::{HtmlElement, HtmlError};
+
+use self::component_dictionary::get_component;
 
 mod builtins;
 mod component_dictionary;
@@ -17,7 +21,7 @@ impl From<HtmlError> for RenderError {
     }
 }
 
-pub type RenderParams = HashMap<String, serde_json::Value>;
+pub type RenderParams = Map<String, JsonValue>;
 pub type RenderResult = Result<HtmlElement, RenderError>;
 pub type RenderFuture = Pin<Box<dyn Future<Output = RenderResult>>>;
 
@@ -33,4 +37,20 @@ pub trait AsyncComponent: Send + Sync {
 pub enum Component {
     Sync(Arc<dyn SyncComponent>),
     Async(Arc<dyn AsyncComponent>),
+}
+
+pub async fn render_composition(composition: JsonValue) -> RenderResult {
+    let Some(JsonValue::String(component_name)) = composition.get("component") else {
+        return Err(RenderError::BadParams);
+    };
+    let Some(JsonValue::Object(params)) = composition.get("params") else {
+        return Err(RenderError::BadParams);
+    };
+    let Ok(component) = get_component(&component_name).await else {
+        return Err(RenderError::BadParams);
+    };
+    match component {
+        Component::Async(component) => component.render(params.clone()).await,
+        Component::Sync(component) => component.render(params.clone()),
+    }
 }

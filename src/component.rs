67bae07e34763dc5132d4aa245864bd0a0f2d8b4
lechos22 <63498252+lechos22@users.noreply.github.com>
2024-03-16 19:12:50 +0100

    Create `Component` trait

---
+++ src/component.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+use crate::html::{HtmlElement, HtmlError};
+
+pub enum RenderError {
+    BadParams,
+    HtmlError(HtmlError),
+    Unknown,
+}
+
+impl From<HtmlError> for RenderError {
+    fn from(html_error: HtmlError) -> Self {
+        Self::HtmlError(html_error)
+    }
+}
+
+pub type RenderParams = HashMap<String, serde_json::Value>;
+
+pub trait Component {
+    fn render(&self, params: RenderParams) -> Result<HtmlElement, RenderError>;
+}

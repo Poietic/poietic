061f98586aec3b67082aa35ec6ff97dbe76179ca
lechos22 <63498252+lechos22@users.noreply.github.com>
2024-03-22 10:35:43 +0100

    Create `DatabaseError` type

--- src/database/connection.rs
+++ src/database/connection.rs
@@ -8,34 +8,38 @@ use tokio::sync::{Mutex, OnceCell};
 
 use crate::config::get_config;
 
+use super::database_error::DatabaseError;
+
 pub struct ConnectionManager {
     connections: Mutex<Vec<Surreal<Any>>>,
     address: String,
 }
 
 impl ConnectionManager {
-    async fn new(address: &str, pool_size: usize) -> Arc<Self> {
+    async fn new(address: &str, pool_size: usize) -> Result<Arc<Self>, DatabaseError> {
         let mut connections = Vec::<Surreal<Any>>::with_capacity(pool_size);
         for _ in 0..pool_size {
-            let connection = any::connect(address).await.unwrap();
+            let connection = any::connect(address).await?;
             connections.push(connection)
         }
-        Arc::new(Self {
+        let connection_manager = Self {
             connections: Mutex::new(connections),
             address: address.to_string(),
-        })
+        };
+        Ok(Arc::new(connection_manager))
     }
-    async fn get_connection(self: Arc<Self>) -> ConnectionHandle {
-        match self.connections.lock().await.pop() {
+    async fn get_connection(self: Arc<Self>) -> Result<ConnectionHandle, DatabaseError> {
+        let connection_handle = match self.connections.lock().await.pop() {
             Some(connection) => ConnectionHandle {
                 connection,
                 connection_manager: Some(self.clone()),
             },
             None => ConnectionHandle {
-                connection: any::connect(&self.address).await.unwrap(),
+                connection: any::connect(&self.address).await?,
                 connection_manager: None,
             },
-        }
+        };
+        Ok(connection_handle)
     }
     fn put_connection(self: Arc<Self>, connection: Surreal<Any>) {
         let self_clone = self.clone();
@@ -67,7 +71,7 @@ impl Drop for ConnectionHandle {
 static CONNECTION_MANAGER: OnceCell<Arc<ConnectionManager>> = OnceCell::const_new();
 
 async fn create_connection_manager() -> Arc<ConnectionManager> {
-    ConnectionManager::new(&get_config().database.address, 4).await
+    ConnectionManager::new(&get_config().database.address, 4).await.unwrap()
 }
 
 async fn get_connection_manager() -> Arc<ConnectionManager> {
@@ -77,6 +81,6 @@ async fn get_connection_manager() -> Arc<ConnectionManager> {
         .clone()
 }
 
-pub async fn get_connection() -> ConnectionHandle {
+pub async fn get_connection() -> Result<ConnectionHandle, DatabaseError> {
     get_connection_manager().await.get_connection().await
 }

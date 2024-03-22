6821bdf16515cab83fc573d8b702cc2e09f76787
lechos22 <63498252+lechos22@users.noreply.github.com>
2024-03-22 20:50:02 +0100

    Split pooled and unpooled connection manager

--- src/database/connection.rs
+++ src/database/connection.rs
@@ -10,27 +10,23 @@ use crate::config::get_config;
 
 use super::database_error::DatabaseError;
 
-pub struct ConnectionManager {
+#[derive(Debug)]
+pub struct PooledConnectionManager {
     connections: Mutex<Vec<Surreal<Any>>>,
     address: String,
 }
 
-impl ConnectionManager {
+impl PooledConnectionManager {
     async fn new(address: &str, pool_size: usize) -> Result<Arc<Self>, DatabaseError> {
         let mut connections = Vec::<Surreal<Any>>::with_capacity(pool_size);
         for _ in 0..pool_size {
             connections.push(Self::create_connection(address).await?)
         }
-        let connection_manager = Self {
+        let connection_manager = PooledConnectionManager {
             connections: Mutex::new(connections),
             address: address.to_string(),
         };
         Ok(Arc::new(connection_manager))
-    }
-    async fn create_connection(address: &str) -> Result<Surreal<Any>, DatabaseError> {
-        let connection = any::connect(address).await?;
-        connection.use_ns("poietic").use_db("poietic").await?;
-        Ok(connection)
     }
     async fn get_connection(self: Arc<Self>) -> Result<ConnectionHandle, DatabaseError> {
         let connection_handle = match self.connections.lock().await.pop() {
@@ -45,6 +41,11 @@ impl ConnectionManager {
         };
         Ok(connection_handle)
     }
+    async fn create_connection(address: &str) -> Result<Surreal<Any>, DatabaseError> {
+        let connection = any::connect(address).await?;
+        connection.use_ns("poietic").use_db("poietic").await?;
+        Ok(connection)
+    }
     fn release_connection(self: Arc<Self>, connection: Surreal<Any>) {
         let self_clone = self.clone();
         tokio::spawn(async move {
@@ -58,8 +59,52 @@ impl ConnectionManager {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct UnpooledConnectionManager {
+    connection: Surreal<Any>,
+}
+
+impl UnpooledConnectionManager {
+    fn get_connection(&self) -> Result<ConnectionHandle, DatabaseError> {
+        Ok(ConnectionHandle {
+            connection_manager: None,
+            connection: self.connection.clone(),
+        })
+    }
+
+    async fn new(address: &str) -> Result<Self, DatabaseError> {
+        Ok(Self {
+            connection: any::connect(address).await?,
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum ConnectionManager {
+    Pooled(Arc<PooledConnectionManager>),
+    Unpooled(UnpooledConnectionManager),
+}
+
+impl ConnectionManager {
+    pub async fn new(address: &str, pool_size: Option<usize>) -> Result<Self, DatabaseError> {
+        let connection_manager = match pool_size {
+            Some(pool_size) => {
+                Self::Pooled(PooledConnectionManager::new(address, pool_size).await?)
+            }
+            None => Self::Unpooled(UnpooledConnectionManager::new(address).await?),
+        };
+        Ok(connection_manager)
+    }
+    async fn get_connection(&self) -> Result<ConnectionHandle, DatabaseError> {
+        match self {
+            Self::Pooled(pooled) => pooled.clone().get_connection().await,
+            Self::Unpooled(unpooled) => unpooled.get_connection(),
+        }
+    }
+}
+
 pub struct ConnectionHandle {
-    connection_manager: Option<Arc<ConnectionManager>>,
+    connection_manager: Option<Arc<PooledConnectionManager>>,
     connection: Surreal<Any>,
 }
 
@@ -81,19 +126,19 @@ impl Drop for ConnectionHandle {
     }
 }
 
-static CONNECTION_MANAGER: OnceCell<Arc<ConnectionManager>> = OnceCell::const_new();
+pub(super) static CONNECTION_MANAGER: OnceCell<ConnectionManager> = OnceCell::const_new();
 
-async fn create_connection_manager() -> Arc<ConnectionManager> {
-    ConnectionManager::new(&get_config().database.address, 4)
+async fn create_connection_manager() -> ConnectionManager {
+    let config = &get_config().database;
+    ConnectionManager::new(&config.address, config.pool_size)
         .await
         .unwrap()
 }
 
-async fn get_connection_manager() -> Arc<ConnectionManager> {
+async fn get_connection_manager() -> &'static ConnectionManager {
     CONNECTION_MANAGER
         .get_or_init(create_connection_manager)
         .await
-        .clone()
 }
 
 pub async fn get_connection() -> Result<ConnectionHandle, DatabaseError> {

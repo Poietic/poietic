837dc81c8d083d02b65023bca4f73ed39d8f06f0
lechos22 <63498252+lechos22@users.noreply.github.com>
2024-03-22 09:06:31 +0100

    Update config structure

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,13 @@
+use std::sync::OnceLock;
+
+use self::base_config::BaseConfig;
+
 pub mod base_config;
 pub mod database_config;
 pub mod http_server_config;
+
+static CONFIG: OnceLock<BaseConfig> = OnceLock::new();
+
+pub fn get_config() -> &'static BaseConfig {
+    CONFIG.get_or_init(|| BaseConfig::load().unwrap())
+}
--- src/config/database_config.rs
+++ src/config/database_config.rs
@@ -1,11 +1,6 @@
-use std::net::IpAddr;
-
 use serde::Deserialize;
 
 #[derive(Clone, Deserialize)]
 pub struct DatabaseConfig {
-    pub address: IpAddr,
-    pub port: u16,
-    pub user: String,
-    pub password: String
+    pub address: String
 }

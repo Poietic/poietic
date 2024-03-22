c64f3ea959545f81814b7b3ea96cd35279175855
lechos22 <63498252+lechos22@users.noreply.github.com>
2024-03-22 18:50:26 +0100

    Clean up `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,13 @@ mod database;
 mod html;
 mod server;
 
-
-
 #[actix_web::main]
 async fn main() -> Result<(), JoinError> {
     let mut http_server_set = JoinSet::new();
     http_server_set.spawn(start_public_http_server());
     http_server_set.spawn(start_admin_http_server());
     while let Some(res) = http_server_set.join_next().await {
-        let _ = res?;
+        res?;
     }
     Ok(())
 }

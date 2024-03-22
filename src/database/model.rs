cd2dd599c2ed78c787838edc5bebf7ab188baf2e
lechos22 <63498252+lechos22@users.noreply.github.com>
2024-03-22 11:03:01 +0100

    Make fields in database models public

--- src/database/model.rs
+++ src/database/model.rs
@@ -4,22 +4,22 @@ use surrealdb::sql::{Datetime, Thing};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct User {
-  id: Thing,
-  email: String,
-  password_hash: String,
-  password_salt: String,
+  pub id: Thing,
+  pub email: String,
+  pub password_hash: String,
+  pub password_salt: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Composition {
-  id: Thing,
-  content: JsonValue,
-  last_modified_at: Datetime,
+  pub id: Thing,
+  pub content: JsonValue,
+  pub last_modified_at: Datetime,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Page {
-  id: Thing,
-  path: String,
-  composition: Thing,
+  pub id: Thing,
+  pub path: String,
+  pub composition: Thing,
 }

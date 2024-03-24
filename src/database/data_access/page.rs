b917c5bb9fd091b96c3b649f8b483d610e83b637
lechos22 <63498252+lechos22@users.noreply.github.com>
2024-03-24 11:48:23 +0100

    Implement page creation

--- src/database/data_access/page.rs
+++ src/database/data_access/page.rs
@@ -1,5 +1,11 @@
+use std::str::FromStr;
+
+use surrealdb::sql::{Id, Thing};
+
 use crate::database::{
-    connection::{connection_handle::ConnectionHandle, connection_manager::get_connection_manager}, database_error::DatabaseError, model::Page
+    connection::{connection_handle::ConnectionHandle, connection_manager::get_connection_manager},
+    database_error::DatabaseError,
+    model::Page,
 };
 
 pub(super) async fn get_page_at_path_on_connection(
@@ -20,4 +26,27 @@ pub(super) async fn get_page_at_path_on_connection(
 pub async fn get_page_at_path(path: &str) -> Result<Page, DatabaseError> {
     let connection = get_connection_manager().await.get_connection().await?;
     get_page_at_path_on_connection(connection, path).await
+}
+
+pub(super) async fn create_page_on_connection(
+    connection: ConnectionHandle,
+    path: String,
+    composition_id: String,
+) -> Result<Page, DatabaseError> {
+    let id = Thing::from(("page", Id::ulid()));
+    let page = Page {
+        id,
+        composition: Thing::from_str(&composition_id).unwrap(),
+        path,
+    };
+    connection
+        .create::<Option<Page>>(&page.id)
+        .content(&page)
+        .await?;
+    Ok(page)
+}
+
+pub async fn create_page(path: String, composition_id: String) -> Result<Page, DatabaseError> {
+    let connection = get_connection_manager().await.get_connection().await?;
+    create_page_on_connection(connection, path, composition_id).await
 }
--- src/server/admin/api/create_page.rs
+++ src/server/admin/api/create_page.rs
@@ -1,14 +1,25 @@
 use actix_web::{post, web::Json, HttpResponse, Responder};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+
+use crate::error::PoieticError;
 
 #[derive(Debug, Deserialize)]
-struct CreatePageBody {
+struct CreatePageRequestBody {
     path: String,
     composition_id: String,
 }
 
+#[derive(Debug, Serialize)]
+struct CreatePageResponseBody {
+    id: String,
+}
+
 #[post("/poietic/create-page")]
-async fn create_page(body: Json<CreatePageBody>) -> impl Responder {
-    todo!("Create the page");
-    HttpResponse::Ok()
+async fn create_page(body: Json<CreatePageRequestBody>) -> Result<impl Responder, PoieticError> {
+    let body = body.into_inner();
+    use crate::database::data_access::page::create_page;
+    let page = create_page(body.path, body.composition_id).await?;
+    Ok(HttpResponse::Ok().json(CreatePageResponseBody {
+        id: page.id.to_string(),
+    }))
 }

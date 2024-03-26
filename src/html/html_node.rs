785ff4e5ef9d1bfc5636639554a944a0826b3f56
lechos22 <63498252+lechos22@users.noreply.github.com>
2024-03-26 16:31:41 +0100

    Add GNU GPL 2.0 as the project's license

--- src/html/html_node.rs
+++ src/html/html_node.rs
@@ -1,3 +1,15 @@
+// Copyright 2024 Lech Mazur
+//
+// This file is part of Poietic.
+//
+// Poietic is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License, version 2,
+// as published by the Free Software Foundation.
+//
+// Poietic is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with Poietic. If not, see <https://www.gnu.org/licenses/>.
+
 use std::collections::HashMap;
 
 use super::{
